67588f4496413f62c3319e464ab503448d93c741
satoyuki <yuki01377@gmail.com>
2020-06-02 20:49:18 +0900

    test API endpoints

---
+++ mean-contactlist-angular2/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectID = mongodb.ObjectID;
+
+const CONTACTS_COLLECTION = "contacts";
+
+const app = express();
+app.use(bodyParser.json());
+
+const db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  db = client.db();
+  console.log("Database connection ready");
+
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// error handler
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({ "error": message });
+}
+
+app.get("/api/contacts", function (req, res) {  
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  })
+});
+app.post("/api/contacts", function (req, res) {  
+  const newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
+app.get("/api/contacts/:id", function (req, res) {  
+});
+app.put("/api/contacts/:id", function (req, res) {  
+});
+app.delete("/api/contacts/:id", function (req, res) {  
+});
